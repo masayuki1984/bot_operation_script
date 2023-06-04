5d96eb7355fed5dc112c1d270477dcb119ba4dc2
masayuki1984 <xianx.standalone@gmail.com>
2023-06-04 22:24:41 +0900

    configファイルの参照PATHを修正

--- bitflyer/get_bitflyer_FX_BTC_ohlc.py
+++ bitflyer/get_bitflyer_FX_BTC_ohlc.py
@@ -6,7 +6,7 @@ import pandas as pd
 import yaml
 
 def fetch_yaml_config() -> dict:
-    config = "/script/bitflyer/config.yml"
+    config = "/script/config.yml"
     with open(config, "r", encoding="utf-8") as yml:
         config = yaml.safe_load(yml)
     return config
